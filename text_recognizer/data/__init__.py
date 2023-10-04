75a8f5b41983fd0cb53947bae4031e14a584ab62
Ziyue Xu <xuziyue6@gmail.com>
2023-10-04 16:09:45 -0400

    add cnn_lstm models to recognize the whole line

--- text_recognizer/data/__init__.py
+++ text_recognizer/data/__init__.py
@@ -2,6 +2,11 @@ from .util import BaseDataset
 from .base_data_module import BaseDataModule
 from .mnist import MNIST
 
+# Hide lines below until Lab 2
+from .emnist import EMNIST
+from .emnist_lines import EMNISTLines
+
+# Hide lines above until Lab 2
 
 
 
